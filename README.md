ec1ac969ef32aeba91e47f5c4c9ceb1928b8348c
Austin <jacobsonaustinj@gmail.com>
2016-09-06 23:12:51 -0400

    conv net and real preprocessing

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ Seizure prediction model from Kaggle's iEEG Data. For https://www.kaggle.com/c/m
 - 2014 Kaggle competition with Dog iEEG's: https://www.kaggle.com/c/seizure-prediction
 - https://github.com/MichaelHills/seizure-detection
 - Signal Processing for Machine Learning: https://www.youtube.com/watch?v=VO0d6EuGpO0
+- Time Series Shapelets: http://delivery.acm.org/10.1145/1560000/1557122/p947-ye.pdf?ip=128.227.226.41&id=1557122&acc=ACTIVE%20SERVICE&key=5CC3CBFF4617FD07%2EC2A817F22E85290F%2E4D4702B0C3E38B35%2E4D4702B0C3E38B35&CFID=664809632&CFTOKEN=98072365&__acm__=1473189592_e7e9e1b9abf2c9e4f2b992e92661a55b
--- data_utils.py
+++ data_utils.py
@@ -5,66 +5,51 @@ import os
 import re
 from glob import glob
 import numpy as np
+import multiprocessing as mp
 
-class IEEG_Data:
 
-    def __init__(self, dirpath=os.path.abspath(os.path.join('E:', 'Seizure_Data'))):
-        self.dirpath = dirpath
+def rolling_window(a, window):
+    shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
+    strides = a.strides + (a.strides[-1],)
+    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
 
-    def preprocess(self, x):
-        return signal.resample(x, 600, window=400).flatten('F')
+def resample(x):
+    return signal.resample(x, 600, window=400).astype("float32").flatten('F')
 
-    def get_label(self, path):
-        '''
-        splits file name into patient, sample, label
-        '''
-        fname = os.path.basename(path)
-        split = re.sub('.mat', '', fname).split('_')
-        return split
+def get_label(path):
+    fname = os.path.basename(path)
+    split = re.sub('.mat', '', fname).split('_')
+    return int(split[2])
 
-    def read_file(self, path, train=True):
+def read_file(path):
+    try:
         mat = loadmat(path)
         names = mat['dataStruct'].dtype.names
         ndata = {n: mat['dataStruct'][n][0, 0] for n in names}
         data = ndata['data']
-        if train:
-            _, _, label = self.get_label(path)
-            return data, label
-        else:
-            return data
+        return data
+    except Exception as e:
+        print os.path.basename(path)
+        print e
 
-    def load(self, dirname):
-        path = os.path.join(self.dirpath, dirname, '*.mat')
-        files = glob(path)
-        if 'train' in dirname:
-            X = []
-            y = []
-            i = 1
-            for f in files:
-                if i % 100 == 0:
-                    print "loading file ", i, " of ", len(files)
-                try:
-                    x_i, y_i = self.read_file(f)
-                    x_i = self.preprocess(x_i)
-                    X.append(x_i)
-                    y.append(y_i)
-                    i += 1
-                except Exception as e:
-                    print f
-                    print e
-            return np.array(X), np.array(y), map(os.path.basename, files)
-        else:
-            for f in files:
-                if i % 100 == 0:
-                    print "loading file ", i, " of ", len(files)
-                try:
-                    x_i = self.read_file(f, train=False)
-                    x_i = self.preprocess(x_i)
-                    X.append(x_i)
-                    i += 1
-                except Exception as e:
-                    print f
-                    print e
-            return np.array(X), map(os.path.basename, files)
+def load_data(dirname, shape="flat", processes=mp.cpu_count()-1, dirpath=os.path.abspath(os.path.join('E:', 'Seizure_Data')), sample=False):
+    path = os.path.join(dirpath, dirname, '*.mat')
+    files = glob(path)
+    if sample == True:
+        files = files[0:20]
+    print "loading", len(files), "with", processes, "processes."
+    p = mp.Pool(processes)
+    X = np.array(p.map(read_file, files))
+    files = np.array(p.map(os.path.basename, files))
+    if shape == 'cnn':
+        X = X.reshape((len(X), 16, 600, 1))  # same as n x channels x w x h
+    if 'train' in dirname:
+        y = np.array(p.map(get_label, files))
+        return X, y, files
+    return X, files
 
-if __name__ == '__main__':
+
+
+
+if __name__=='__main__':
+    pass
